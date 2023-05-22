06bebc5c587655f75719a8fb67e0df2d4ae79b36
anashamimi <anashamimi2@gmail.com>
2023-05-22 18:11:53 +0200

    add cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,27 @@
     <div class="ah-jumbotron">
 
     </div>
+    <div class="container">
+
+            <div class="ah-current bg-primary text-uppercase fw-bold fs-5 p-2 d-inline-block">
+                current series
+            </div>
+
+            <div class="row py-4">
+
+                @foreach ($comics as $comic)
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-2 gy-5">
+                        <div class="ah-card">
+                            <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}">
+                            <p>{{$comic['series']}}</p>
+                    </div>
+                    </div>
+                @endforeach
+
+            </div>
+
+            <div class="p-5 d-flex justify-content-center">
+                <button type="button" class="btn btn-primary rounded-0 px-5 text-uppercase">load more</button>
+            </div>
+        </div>
 @endsection
